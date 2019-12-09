ef827f08622c446f3a3ebac5a6b32e6c22299c15
Tsogodar <marcin.gryczon@o2.pl>
2019-12-09 23:31:03 +0100

    refactor lock screen

--- lib/screens/cards_screen.dart
+++ lib/screens/cards_screen.dart
@@ -14,27 +14,30 @@ class CardsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final settingsProvider = Provider.of<Settings>(context);
     final Map<String, dynamic> settings = settingsProvider.settings;
-    List<String> cardSet =
-        settings['lock'] == false ? cardSets[0] : [settings['lock']];
     return Scaffold(
       appBar: CustomAppBar(),
       body: Container(
         color: Colors.lightBlueAccent,
-        child: new Swiper(
-          itemBuilder: (BuildContext context, int index) {
-            return CardItem(cardSet[index]);
-          },
-          scrollDirection:
-              settings['vertical_swipe'] ? Axis.vertical : Axis.horizontal,
-          loop: settings['loop'],
-          itemCount: cardSet.length,
-          containerHeight: 100,
-          itemWidth: 300,
-          itemHeight: 400,
-          layout: settings['alternative_layout']
-              ? SwiperLayout.STACK
-              : SwiperLayout.TINDER,
-        ),
+        child: settings['lock'] == false
+            ? new Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return CardItem(cardSets[0][index]);
+                },
+                scrollDirection: settings['vertical_swipe']
+                    ? Axis.vertical
+                    : Axis.horizontal,
+                loop: settings['loop'],
+                itemCount: cardSets[0].length,
+                containerHeight: 100,
+                itemWidth: 300,
+                itemHeight: 400,
+                layout: settings['alternative_layout']
+                    ? SwiperLayout.STACK
+                    : SwiperLayout.TINDER,
+              )
+            : Container(
+                child: CardItem(settings['lock']),
+              ),
       ),
     );
   }
