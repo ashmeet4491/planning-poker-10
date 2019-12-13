d2ca12e39999f3615a30c2d6fb923dd3295586c5
Tsogodar <marcin.gryczon@o2.pl>
2019-12-13 21:23:55 +0100

    store user settings

--- lib/providers/settings.provider.dart
+++ lib/providers/settings.provider.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings with ChangeNotifier {
-  Map<String, dynamic> _settings = {
+  
+  SharedPreferences _prefs;
+
+  Map<String, dynamic> _default_settings = {
     'vertical_swipe': false,
     'loop': true,
     'favorite': '8',
     'lock': false,
-    'alternative_layout':false
+    'alternative_layout': false
   };
+
+  Map<String, dynamic> _settings = const {
+    'vertical_swipe': false,
+    'loop': true,
+    'favorite': '8',
+    'lock': false,
+    'alternative_layout': false
+  };
+
+  Settings() {
+    loadSettings();
+  }
+
+  loadSettings() async {
+    _prefs = await SharedPreferences.getInstance();
+    if (_prefs.getString('data') == null) {
+      _prefs.setString(
+        'data',
+        jsonEncode(_default_settings),
+      );
+      _settings = _default_settings;
+      notifyListeners();
+    } else {
+      _settings = jsonDecode(_prefs.getString('data'));
+      notifyListeners();
+    }
+  }
+
   Map<String, dynamic> get settings {
     return {..._settings};
   }
 
   void changeSettings(String key, dynamic value) {
     _settings[key] = value;
+     _prefs.setString(
+        'data',
+        jsonEncode(_settings),
+      );
     notifyListeners();
   }
 
