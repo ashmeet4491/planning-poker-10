5bff94d2ca5bf4a8e7369b5fd6cbfbb9ccae58f3
Ashmeet <ashmeetk123@gmail.com>
2020-05-09 14:09:21 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:planning_poker/home.dart';
+import 'package:planning_poker/load.dart';
 import 'package:provider/provider.dart';
+import 'package:planning_poker/home.dart';
+import 'package:planning_poker/screens/create_room.dart';
+import 'package:planning_poker/load.dart';
 
 import './screens/settings_screen.dart';
 import './screens/cards_screen.dart';
@@ -12,12 +17,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
+
       builder: (context)=>Settings(),
       child: MaterialApp(
+        initialRoute: '/home',
         title: 'Planning Poker',
         routes: {
+          '/load':(ctx)=>load(),
+
+          '/home':(ctx)=>Home(),
           '/': (ctx) => CardsScreen(),
           '/settings': (ctx) => SettingsScreen(),
+          '/create_room':(ctx)=>create_room(),
+
         },
       ),
     );
